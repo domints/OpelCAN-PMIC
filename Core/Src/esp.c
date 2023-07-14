eae3e817b40a6c86cdbc3644eeab094d5e7aa7d9
Dominik Szymański <domints@gmail.com>
2023-07-14 13:28:34 +0200

    Add command parsing

--- Core/Src/esp.c
+++ Core/Src/esp.c
@@ -22,6 +22,7 @@ uint8_t text_ix = 0;
 uint8_t msgCnt = 0;
 
 void _esp_parse_command();
+void _esp_process_uart_byte(uint8_t data);
 
 void esp_reset() {
 	mode = UART_MODE_RESET;
@@ -30,80 +31,86 @@ void esp_reset() {
 void esp_receive_uart() {
 	int available = uart_data_available();
 	if (available) {
-		uint8_t txdata[2] = { available & 0xFF, msgCnt };
-		can_tx_send_packet(0x001, txdata, 2);
-
-		msgCnt++;
-
-		uint8_t txdata2[1] = { 0x69 };
-		can_tx_send_packet(0x002, txdata2, 1);
+		uint8_t txdata2[5] = { mode, command, size >> 8 & 0xFF, size & 0xFF, text_ix };
+		can_tx_send_packet(0x001, txdata2, 5);
 
 		for (int i = 0; i < available; i++) {
-			uart_get_byte();
+			uint8_t value = uart_get_byte() & 0xFF;
+			_esp_process_uart_byte(value);
 		}
 	}
 }
 
-void stuff() {
-	/*uint16_t readIx = 0;
-		while (readIx < Size) {
-			if (mode == UART_MODE_RESET) {
-				if (reset_ix == 0) {
-					if (uart_byte_buf[readIx++] == 0xAA)
-						reset_ix++;
-				} else if (reset_ix == 1) {
-					if (uart_byte_buf[readIx++] == 0xFF)
-						reset_ix++;
-					else
-						reset_ix = 0;
-				} else if (reset_ix == 2) {
-					if (uart_byte_buf[readIx++] == 0x55)
-						reset_ix++;
-					else
-						reset_ix = 0;
-				} else if (reset_ix == 3) {
-					if (uart_byte_buf[readIx++] == 0x00)
-						reset_ix++;
-					else
-						reset_ix = 0;
-				}
+void _esp_process_uart_byte(uint8_t value) {
+	if (mode == UART_MODE_RESET) {
+		if (reset_ix == 0) {
+			if (value == 0xAA)
+				reset_ix++;
+		} else if (reset_ix == 1) {
+			if (value == 0xFF)
+				reset_ix++;
+			else
+				reset_ix = 0;
+		} else if (reset_ix == 2) {
+			if (value == 0x55)
+				reset_ix++;
+			else
+				reset_ix = 0;
+		} else if (reset_ix == 3) {
+			if (value == 0x00)
+				reset_ix++;
+			else
+				reset_ix = 0;
+		}
 
-				if (reset_ix == 4) {
-					mode = UART_MODE_WAIT;
-					command = UART_MODE_WAIT;
-				}
-			} else if (command == 0x00) {
-				command = uart_byte_buf[readIx++];
-				if (command > 0x80 && command < 0x90) {
-					mode = UART_MODE_READ_SIZE;
-					size_ix = 0;
-				} else {
-					command = 0x00;
-				}
-			} else {
-				if (mode == UART_MODE_READ_SIZE) {
-					size_buf[size_ix++] = uart_byte_buf[readIx++];
+		if (reset_ix == 4) {
+			mode = UART_MODE_WAIT;
+			command = UART_MODE_WAIT;
+		}
+	} else if (command == 0x00) {
+		command = value;
+		if (command > 0x80 && command < 0x90) {
+			mode = UART_MODE_READ_SIZE;
+			size_ix = 0;
+		} else {
+			command = 0x00;
+		}
+	} else {
+		if (mode == UART_MODE_READ_SIZE) {
+			size_buf[size_ix++] = value;
 
-					if (size_ix == 2) {
-						size = size_buf[1] << 8 | size_buf[0];
-						mode = UART_MODE_READ_TEXT;
-						text_ix = 0;
-					}
-				} else if (mode == UART_MODE_READ_TEXT) {
-					if (text_ix < 128) {
-						text_buf[text_ix] = uart_byte_buf[readIx++];
-					}
-
-					text_ix++;
-
-					if (text_ix >= size) {
-						_uart_parse_command();
-					}
-				}
+			if (size_ix == 2) {
+				size = size_buf[1] << 8 | size_buf[0];
+				mode = UART_MODE_READ_TEXT;
+				text_ix = 0;
 			}
-		}*/
+		} else if (mode == UART_MODE_READ_TEXT) {
+			if (text_ix < 128) {
+				text_buf[text_ix] = value;
+			}
+
+			text_ix++;
+
+			if (text_ix >= size) {
+				_esp_parse_command();
+			}
+		}
+	}
 }
 
+uint16_t title_sz;
+uint16_t album_sz;
+uint16_t artist_sz;
+uint8_t debug_ctr;
+void _esp_debug_size() {
+	uint8_t txdata2[7] = {
+			debug_ctr++,
+			title_sz >> 8 & 0xFF, title_sz & 0xff,
+			album_sz >> 8 & 0xFF, album_sz & 0xff,
+			artist_sz >> 8 & 0xFF, artist_sz & 0xff
+	};
+	can_tx_send_packet(0x002, txdata2, 7);
+}
 
 void _esp_parse_command() {
 	if (size > 128)
@@ -111,12 +118,15 @@ void _esp_parse_command() {
 
 	switch (command) {
 	case CMD_TITLE:
+		_esp_debug_size();
 		can_tx_set_title(text_buf, size);
 		break;
 	case CMD_ARTIST:
+		_esp_debug_size();
 		can_tx_set_artist(text_buf, size);
 		break;
 	case CMD_ALBUM:
+		_esp_debug_size();
 		can_tx_set_album(text_buf, size);
 		break;
 	default:
